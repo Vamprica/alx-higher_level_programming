d446127582377d95704c8c76012c9fa5d296f92a
Vamprica <oluwracheal@gmail.com>
2024-03-12 14:58:45 -0700

    exceptions

---
+++ 0x05-python-exceptions/6-main.py
@@ -0,0 +1,8 @@
+guillaume@ubuntu:~/0x05$ cat 6-main.py
+#!/usr/bin/python3
+raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
+
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
